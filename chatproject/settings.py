bbaf890ade60e9657d538cbd360b8664485b62a7
vishureddy182 <kondamvishnu2001@gmail,com>
2026-05-08 23:09:35 +0530

    settigssss

--- chatproject/settings.py
+++ chatproject/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
+SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
 
 STATICFILES_DIRS = [
     BASE_DIR / "chatapp/static",
